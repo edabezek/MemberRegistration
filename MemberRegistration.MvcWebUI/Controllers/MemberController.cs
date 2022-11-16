c6916e31d24d61e5593ee93f7de32619d7c8b5ae
eda.bezek <eda_bezek@hotmail.com>
2022-11-16 19:18:45 +0300

    Exceptions

--- MemberRegistration.MvcWebUI/Controllers/MemberController.cs
+++ MemberRegistration.MvcWebUI/Controllers/MemberController.cs
@@ -1,5 +1,6 @@
 ﻿using MemberRegistration.Businnes.Abstract;
 using MemberRegistration.Entities.Concrete;
+using MemberRegistration.MvcWebUI.Filters;
 using MemberRegistration.MvcWebUI.Models;
 using System;
 using System.Collections.Generic;
@@ -25,6 +26,7 @@ namespace MemberRegistration.MvcWebUI.Controllers
         }
         // POST: /Member/Add kayıtlar buradan gidecek arayuzden
         [HttpPost]
+        [ExceptionHandler]
         public ActionResult Add(Member member)
         {
             _memberService.AddMember(member);   
